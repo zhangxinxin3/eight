e6e92b75545119d1ea0dba68c15b4b556f2968ee
njx <1043006908@qq.com>
2019-07-10 21:17:22 +0800

    详情

--- jinayoupinapp/src/service/shopDetail.js
+++ jinayoupinapp/src/service/shopDetail.js
@@ -1,4 +1,63 @@
 import request from "@/utils/request";
+//获取详情swiper图片
 export let getDetail = params =>{
     return request.post('/api/open/product/info/1.0.0?pid='+params)
 }
+//获取选择的颜色还是规格
+export let getChoose = params =>{
+    // console.log('params',params)
+    return request.post('/api/open/product/sku/attrs/1.0.0?pid='+params)
+}
+//获取提示邮费地址
+export let getAddress = params =>{
+    return request.post('/api/open/shipping/templates/info/1.0.0?sstid='+params)
+}
+//获取详情下边的图片
+export let getDownPic = params =>{
+    return request.post('/api/open/product/detailPicture/1.0.0'+params)
+}
+//未查询到
+export let noneFind = params =>{
+    return request.post('/api/open/product/coupon/productCouponList/1.0.0?pid='+'50408'+'&bid'+'115'+'&uid'+'150'+'&usiid')
+}
+
+//  ****详情数据
+// Request URL: https://upapi.jinaup.com/api/open/product/info/1.0.0
+// Request Method: POST
+// Form Data:{
+//             pid: 36280
+//            }
+//  description:说明
+           
+// ****提示
+// Request URL: https://upapi.jinaup.com/api/open/shipping/templates/info/1.0.0
+// Request Method: POST
+// result: "西藏自治区,新疆维吾尔自治区不发货。
+// Form Data:{
+//             sstid: 22
+//            }
+
+
+// ****提示已下图片
+//  Request URL: https://upapi.jinaup.com/api/open/product/detailPicture/1.0.0
+//  Request Method: POST
+//  Form Data:{
+//               pid: 36280
+//               basePid: 
+//               userIdentity: 
+//            }
+
+
+// ******选择默认 
+// Request URL: https://upapi.jinaup.com/api/open/product/sku/attrs/1.0.0
+// Request Method: POST
+//  Form Data:{
+//              pid: 36551
+//            }
+
+// ****规格
+// Request URL: https://upapi.jinaup.com/api/open/product/sku/attrs/1.0.0
+// Request Method: POST
+//  Form Data:{
+//               pid: 36280
+//            } 
--- jinayoupinapp/src/store/modules/shopDetail.js
+++ jinayoupinapp/src/store/modules/shopDetail.js
@@ -1,7 +1,11 @@
-import { getDetail } from '@/service/shopDetail'
+import { getDetail,getChoose, getAddress, getDownPic } from '@/service/shopDetail'
 
 const state = {
-    getDetailList:[]
+    getDetailList:[],
+    currentpid:'',
+    chooseList:[],
+    hintAddress:'',
+    picDownList:[]
 }
 
 const mutations = {
@@ -9,9 +13,26 @@ const mutations = {
 }
 
 const actions = {
+    //获取详情swiper图片
     async shopItem({commit,state},payload){
+        state.currentpid = payload;
         let data = await getDetail(payload);
         state.getDetailList = data.result;
+    },
+    //获取选择的颜色还是规格
+    async choose({commit,state},payload){
+        let data = await getChoose(state.currentpid);
+        state.chooseList = data.result;
+    },
+    //获取提示的内容
+    async hintContent({commit,state},payload){
+        let data = await getAddress(payload);
+        state.hintAddress = data.result;
+    },
+    //获取详情下边的图片
+    async picDown({commit,state},payload){
+        let data = await getDownPic(payload);
+        state.picDownList = data.result;
     }
 }
 
